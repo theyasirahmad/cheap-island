e7ca458e0e6611fbda28018d54553c1936fb48e5
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2020-12-24 00:36:36 +0500

    All changes

--- src/config/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://grocce.herokuapp.com',
-  responseType: 'json',
-});
--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import authReducer from './AuthReducer';
-// import userReducer from './userReducer';
-const rootReducer = combineReducers({
-  authReducer,
-  //   userReducer,
-});
-
-export default rootReducer;
