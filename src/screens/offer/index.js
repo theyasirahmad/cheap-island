e00534d3a509615eb806071b05959f08fd25790b
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2020-12-29 04:41:36 +0500

    Offer list buttons update

--- src/screens/offer/index.js
+++ src/screens/offer/index.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, FlatList, ImageBackground} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, ImageBackground } from 'react-native';
 import GlobalHeader from '../../Components/GlobalHeader';
 import OfferCard from '../../Components/OfferCard';
-import {OfferList as OFFERLIST} from '../../dummyData/dummyData';
-import {Colors} from '../../constants/theme';
+import { OfferList as OFFERLIST } from '../../dummyData/dummyData';
+import { Colors } from '../../constants/theme';
 
-const Offer = ({navigation}) => {
+const Offer = ({ navigation }) => {
+  const [toggleBtn, setToggleBtn] = useState(true)
   return (
     <View style={styles.container}>
       <GlobalHeader
@@ -17,23 +18,39 @@ const Offer = ({navigation}) => {
         isFavouriteLoading={false}
         RightIcon={true}
       />
+      <View style={styles.topBtnsView}>
+        {/* {toggleBtn ? */}
+          <TouchableOpacity 
+            onPress={()=> setToggleBtn(true)} 
+            style={[styles.btnTop,{backgroundColor: toggleBtn ? Colors.LinearBlue1 : 'transparent' }]}
+          >
+            <Text style={{ color: "#fff" }}>Offers</Text>
+          </TouchableOpacity> 
+          <TouchableOpacity 
+            onPress={()=> setToggleBtn(false)} 
+            style={[styles.btnTop,{backgroundColor: toggleBtn ? 'transparent' : Colors.LinearBlue1}]}
+          >
+            <Text style={{ color: "#fff" }}>Used offers</Text>
+          </TouchableOpacity>
+        {/* } */}
+      </View>
       <FlatList
         showsVerticalScrollIndicator={false}
         numColumns={1}
         data={OFFERLIST}
         keyExtractor={(item) => item.id}
-        renderItem={(itemData) => <OfferCard 
-          img={itemData.item.img} 
-        name={itemData.item.name}
-        descrption={itemData.item.descrption}
-        favourite={itemData.item.favourite}
-        offerAvail={itemData.item.offer}
-        navigation={navigation}
-      />}
+        renderItem={(itemData) => <OfferCard
+          img={itemData.item.img}
+          name={itemData.item.name}
+          descrption={itemData.item.descrption}
+          favourite={itemData.item.favourite}
+          offerAvail={itemData.item.offer}
+          navigation={navigation}
+        />}
       />
-      <ImageBackground 
-        style={{width:100, height:130, position:"absolute", alignSelf:"center", bottom:10, zIndex: -1000000}} 
-        source={require('../../assets/images/inback.png')} 
+      <ImageBackground
+        style={{ width: 100, height: 130, position: "absolute", alignSelf: "center", bottom: 10, zIndex: -1000000 }}
+        source={require('../../assets/images/inback.png')}
         resizeMode='cover'
       />
     </View>
@@ -47,4 +64,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.backgroundBlueColor,
   },
+  topBtnsView: {
+    flexDirection: 'row', height: 40, alignSelf: "center", backgroundColor: "rgba(0,0,0,0.1)",
+    borderRadius: 20, width:240
+  },
+  btnTop: {
+    paddingHorizontal: 15,
+    backgroundColor: Colors.LinearBlue1, width: 120,
+    justifyContent: "center", alignItems: "center", borderRadius: 20
+  }
 });
