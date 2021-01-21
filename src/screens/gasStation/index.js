17963930669af7439df0efe50abe4fe14015e386
YASIR AHMED <yasira640@gmail.com>
2021-01-22 03:49:30 +0500

    location denied toggle working

--- src/screens/gasStation/index.js
+++ src/screens/gasStation/index.js
@@ -8,13 +8,17 @@ import { Colors } from '../../constants/theme'
 import RBSheet from "react-native-raw-bottom-sheet";
 import axios from 'axios';
 import * as Location from 'expo-location';
+import { ActivityIndicator } from 'react-native';
 
 
 const GasStation = () => {
   const [latitude, setLatitude] = useState(null)
   const [longitude, setLongitude] = useState(null)
+  const [loading, setLoading] = useState(true);
 
-  const [paddingTop, setPaddingTop] = useState(0)
+  const [paddingTop, setPaddingTop] = useState(0);
+
+  const [locationDenied, setLocationDenied] = useState(false)
 
   const [gasStations, setGasStations] = useState([]);
   const [viewStation, setViewStation] = useState(
@@ -39,15 +43,29 @@ const GasStation = () => {
   const getMyLocation = () => {
 
     (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+      try {
+
+        let { status } = await Location.requestPermissionsAsync()
+        if (status !== 'granted') {
+          setLocationDenied(true)
+          setLoading(false)
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+        else {
+          let location = await Location.getCurrentPositionAsync({});
+          setLatitude(location.coords.latitude)
+          setLongitude(location.coords.longitude)
+          setLoading(false)
+          setLocationDenied(false)
+        }
+
+      } catch (error) {
+        console.log(error);
+        setLocationDenied(true)
+        setLoading(false)
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLatitude(location.coords.latitude)
-      setLongitude(location.coords.longitude)
 
       // let results = await Location.reverseGeocodeAsync({
       //   latitude: location.coords.latitude,
@@ -146,145 +164,168 @@ const GasStation = () => {
       // isFavouriteLoading={false}
       // RightIcon={true}
       />
-      <View style={styles.viewMapConatiner}>
-        <View style={{ width: "100%", height: Dimensions.get('window').height * 0.5, backgroundColor: "#bbb" }}>
-          {
-            latitude && longitude &&
-            <MapView
-              // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-              showsUserLocation={true}
-              initialRegion={{
-                latitude: latitude,
-                longitude: longitude,
-                latitudeDelta: 0.0422,
-                longitudeDelta: 0.0421,
-              }}
-              style={{
-                position: 'relative',
-                minHeight: '100%',
-                width: '100%',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                marginBottom: 1,
-                borderWidth: 2,
-              }}
-              onMapReady={_onMapReady}
-            >
-              {
-                gasStations.map((item) => {
-                  return (
-                    <Marker
-                      onPress={() => {
-                        openSheet(
-                          item.name,
-                          item.company,
-                          item.bensin95,
-                          item.bensin95_discount,
-                          item.diesel,
-                          item.diesel_discount
-                        )
-                        // setCardSelected(!cardSelected),
-                        // navigation.navigate('DetailDisplay', {
-                        //   name: item.name,
-                        //   description: item.description,
-                        //   img: item.logo,
-                        //   address: item.address,
-                        //   menuCard: []
-                        // })
-                      }}
-                      key={item.geo.lat.toString() + Math.random() * 100 + item.geo.lon.toString()}
-                      coordinate={{
-                        latitude: parseFloat(item.geo.lat),
-                        longitude: parseFloat(item.geo.lon)
-                      }}
-                    />
-                  )
-                })
-              }
+      {
+        loading ?
+          <ActivityIndicator
+            style={{
+            }} color={Colors.LinearBlue1} />
+          :
+          <>
+            {
+              locationDenied ?
+                <View>
+                  <Text style={{ padding: 20 }}>
+                    Location Denied
+                </Text>
+                </View>
+                :
+                <>
+                  <View style={styles.viewMapConatiner}>
+                    <View style={{ width: "100%", height: Dimensions.get('window').height * 0.5, backgroundColor: "#bbb" }}>
+                      {
+                        latitude && longitude &&
+                        <MapView
+                          // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                          showsUserLocation={true}
+                          initialRegion={{
+                            latitude: latitude,
+                            longitude: longitude,
+                            latitudeDelta: 0.0422,
+                            longitudeDelta: 0.0421,
+                          }}
+                          style={{
+                            position: 'relative',
+                            minHeight: '100%',
+                            width: '100%',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            marginBottom: 1,
+                            borderWidth: 2,
+                          }}
+                          onMapReady={_onMapReady}
+                        >
+                          {
+                            gasStations.map((item) => {
+                              return (
+                                <Marker
+                                  onPress={() => {
+                                    openSheet(
+                                      item.name,
+                                      item.company,
+                                      item.bensin95,
+                                      item.bensin95_discount,
+                                      item.diesel,
+                                      item.diesel_discount
+                                    )
+                                    // setCardSelected(!cardSelected),
+                                    // navigation.navigate('DetailDisplay', {
+                                    //   name: item.name,
+                                    //   description: item.description,
+                                    //   img: item.logo,
+                                    //   address: item.address,
+                                    //   menuCard: []
+                                    // })
+                                  }}
+                                  key={item.geo.lat.toString() + Math.random() * 100 + item.geo.lon.toString()}
+                                  coordinate={{
+                                    latitude: parseFloat(item.geo.lat),
+                                    longitude: parseFloat(item.geo.lon)
+                                  }}
+                                />
+                              )
+                            })
+                          }
 
-            </MapView>
-          }
-        </View>
-        {/* <ScrollView style={styles.containerList} showsVerticalScrollIndicator={false}> */}
-        <View style={styles.containerList}>
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            numColumns={1}
-            data={gasStations}
-            keyExtractor={(item) => item.id}
-            renderItem={(itemData) => (
-              <TouchableOpacity onPress={() => openSheet(
-                itemData.item.name,
-                itemData.item.company,
-                itemData.item.bensin95,
-                itemData.item.bensin95_discount,
-                itemData.item.diesel,
-                itemData.item.diesel_discount
-              )}>
-                <GasStationCard
-                  StationName={itemData.item.name}
-                  favourite={itemData.item.favourite}
-                  latitude={latitude}
-                  longitude={longitude}
-                  geo={itemData.item.geo}
-                />
-              </TouchableOpacity>
-            )}
-          />
-        </View>
-        {/* </ScrollView> */}
-      </View>
-      <RBSheet
-        ref={rbSheetRef}
-        height={260}
-        openDuration={250}
-        // closeOnDragDown={true}
-        // dragFromTopOnly={true}
-        customStyles={{
-          container: {
-            justifyContent: "center",
-            borderTopLeftRadius: 20,
-            borderTopRightRadius: 20
-            // alignItems: "center"
-          }
-        }}
-      >
-        <View style={{ flex: 1, padding: 15 }}>
-          <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
-            Company: {viewStation.company}
+                        </MapView>
+                      }
+                    </View>
+                    {/* <ScrollView style={styles.containerList} showsVerticalScrollIndicator={false}> */}
+                    <View style={styles.containerList}>
+                      <FlatList
+                        showsVerticalScrollIndicator={false}
+                        numColumns={1}
+                        data={gasStations}
+                        keyExtractor={(item) => item.id}
+                        renderItem={(itemData) => (
+                          <TouchableOpacity
+                            key={Math.random() * 1000 * Math.random() + 200}
+                            onPress={() => openSheet(
+                              itemData.item.name,
+                              itemData.item.company,
+                              itemData.item.bensin95,
+                              itemData.item.bensin95_discount,
+                              itemData.item.diesel,
+                              itemData.item.diesel_discount
+                            )}>
+                            <GasStationCard
+                              StationName={itemData.item.name}
+                              favourite={itemData.item.favourite}
+                              latitude={latitude}
+                              longitude={longitude}
+                              geo={itemData.item.geo}
+                            />
+                          </TouchableOpacity>
+                        )}
+                      />
+                    </View>
+                    {/* </ScrollView> */}
+                  </View>
+                  <RBSheet
+                    ref={rbSheetRef}
+                    height={260}
+                    openDuration={250}
+                    // closeOnDragDown={true}
+                    // dragFromTopOnly={true}
+                    customStyles={{
+                      container: {
+                        justifyContent: "center",
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20
+                        // alignItems: "center"
+                      }
+                    }}
+                  >
+                    <View style={{ flex: 1, padding: 15 }}>
+                      <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
+                        Company: {viewStation.company}
+                      </Text>
+                      <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
+                        Name: {viewStation.StationName}
+                      </Text>
+
+                      <View style={{ width: '90%', height: 1, backgroundColor: "rgba(0,0,0,0.1)", marginVertical: 15 }} />
+
+                      <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
+                        Bension95
           </Text>
-          <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
-            Name: {viewStation.StationName}
+                      <View style={{ flexDirection: "row" }}>
+                        <Text style={styles.txtDetail}>Price: {viewStation.fuel1}</Text>
+                        <Text style={styles.txtDetail}>Discounted price: {viewStation.discount1}</Text>
+                      </View>
+
+                      <View style={{ width: '90%', height: 1, backgroundColor: "rgba(0,0,0,0.1)", marginVertical: 15 }} />
+
+                      <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
+                        Diesel
           </Text>
+                      <View style={{ flexDirection: "row" }}>
+                        <Text style={styles.txtDetail}>Price: {viewStation.fuel2}</Text>
+                        <Text style={styles.txtDetail}>Discounted price: {viewStation.discount2}</Text>
+                      </View>
+                    </View>
+                  </RBSheet>
+                  <ImageBackground
+                    style={{ width: 100, height: 130, position: "absolute", alignSelf: "center", bottom: 10, zIndex: -1000000 }}
+                    source={require('../../assets/images/inback.png')}
+                    resizeMode="cover"
+                  />
+                </>
+            }
+          </>
+      }
 
-          <View style={{ width: '90%', height: 1, backgroundColor: "rgba(0,0,0,0.1)", marginVertical: 15 }} />
-
-          <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
-            Bension95
-          </Text>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.txtDetail}>Price: {viewStation.fuel1}</Text>
-            <Text style={styles.txtDetail}>Discounted price: {viewStation.discount1}</Text>
-          </View>
-
-          <View style={{ width: '90%', height: 1, backgroundColor: "rgba(0,0,0,0.1)", marginVertical: 15 }} />
-
-          <Text style={{ fontSize: 18, fontWeight: "bold", color: 'rgba(0,0,0,0.4)' }}>
-            Diesel
-          </Text>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.txtDetail}>Price: {viewStation.fuel2}</Text>
-            <Text style={styles.txtDetail}>Discounted price: {viewStation.discount2}</Text>
-          </View>
-        </View>
-      </RBSheet>
-      <ImageBackground
-        style={{ width: 100, height: 130, position: "absolute", alignSelf: "center", bottom: 10, zIndex: -1000000 }}
-        source={require('../../assets/images/inback.png')}
-        resizeMode="cover"
-      />
     </View>
   );
 };
@@ -313,7 +354,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     // maxHeight: Dimensions.get('window').height*0.68,
     width: "100%",
-    marginTop:-20,
+    marginTop: -20,
     borderRadius: 20,
     alignSelf: "center",
     borderWidth: 1,
